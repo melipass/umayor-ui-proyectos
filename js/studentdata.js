f060f076e8e4bddf1ddd05910d664c1e1b3b5f28
melipass <32807254+melipass@users.noreply.github.com>
2020-11-19 02:09:09 -0300

    Added projects page and some JS.

---
+++ js/studentdata.js
@@ -0,0 +1,51 @@
+var career = {
+	students : [
+		{
+			name : "Alice", 
+			thumbnail : "images/alice/photo.jpg",
+			projects : [
+				{	
+					src : "images/alice/1.jpg",
+					title : "Proyecto de Ingeniería I", 
+					description : "Automatización de procesos en empresa consultora."
+				}, 
+				{	
+					src : "images/alice/2.jpg",
+					title : "Proyecto de Ingeniería II", 
+					description : "Bot para automatizar procesos en empresa consultora."
+				}, 
+				{	
+					src : "images/alice/at.jpg",
+					title : "Proyecto Aplicado de Título", 
+					description : "Implementación de bot que automatiza procesos en empresa consultora."
+				}
+			]
+		},
+		{
+			name : "Bob", 
+			thumbnail : "images/bob/photo.jpg",
+			photos : [
+				{	
+					src : "images/bob/1.jpg",
+					title : "Proyecto de Ingeniería I", 
+					description : "Aplicación para alimentar gatitos de la calle"
+				}, 
+				{	
+					src : "images/bob/1-2.jpg",
+					title : "Proyecto de Ingeniería I (2do intento)", 
+					description : "Aplicación para encontrar tiendas abiertas durante la cuarentena."
+				}, 
+				{	
+					src : "images/bob/2.jpg",
+					title : "Proyecto de Ingeniería II", 
+					description : "Aplicación android de búsqueda de tiendas abiertas durante la pandemia."
+                },
+                {	
+					src : "images/bob/at.jpg",
+					title : "Proyecto Aplicado de Título", 
+					description : "Directorio digital de tiendas abiertas durante situaciones de emergencia para teléfonos inteligentes."
+				}
+			]
+		}
+	]
+};
---
+++ js/students.js
@@ -0,0 +1,40 @@
+/* Template to be compiled */
+var students_template, projects_template, project_template;
+
+/* Variables to iterate with */
+var current_student = career.students[0];
+var current_project = current_student.projects[0];
+
+/* Function that instantiates a template */
+function showTemplate(template, data){
+    var html = template(data);
+    $('#content-to-fill').html(html);
+}
+
+/* Function that gets called when the document loaded */
+$(document).ready(function(){
+    var source = $("#students-template").html();
+    students_template = Handlebars.compile(source);
+
+    var source = $("#projects-template").html();
+    projects_template = Handlebars.compile(source);
+
+    var source = $("#project-template").html();
+    project_template = Handlebars.compile(source);
+})
+
+$("#proy1-tab").click(function(){
+    showTemplate(students_template, career);
+    $(".nav-tabs .active").removeClass("active");
+    $("#proy1-tab").addClass("active");
+    $(".student-thumbnail").click(function(){
+        var index = $(this).data("id");
+        current_student = career.students[index];
+        showTemplate(projects_template, current_student);
+        $(".project-thumbnail").click(function(){
+            var index = $(this).data("id");
+            current_project = current_student.projects[index];
+            showTemplate(project_template,current_project);
+        })
+    })
+})
