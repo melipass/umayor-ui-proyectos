e8da9d7ada3d25c39f743beff715bfec1018a2fe
melipass <melissa.silvas@mayor.cl>
2020-11-21 18:22:49 -0300

    Finished projects.html by-student tab.

--- js/studentdata.js
+++ js/studentdata.js
@@ -4,7 +4,7 @@ var career = {
 			name : "Alicia", 
 			surname: "Soto",
 			thumbnail : "images/alicia_soto/photo.jpg",
-			status : "Esperando respuesta de Profesor Revisor.",
+			status : 'Esperando respuesta de Profesor Revisor <ion-icon name="time-outline" class="mr-2 lead text-orange"></ion-icon>',
 			cohort: "2016",
 			curriculum: "ICC110-2014",
 			career: "Ingeniería Civil en Computación e Informática",
@@ -15,24 +15,45 @@ var career = {
 			projects : [
 				{	
 					src : "images/alicia_soto/1.jpg",
-					title : "Proyecto de Ingeniería I", 
-					description : "Automatización de procesos en empresa consultora.",
+					course : "[2019-02] Proyecto de Ingeniería I", 
+					title : "Automatización de procesos en empresa consultora",
 					status: "Aprobado",
-					bscolor: "success"
+					bscolor: "success",
+					academic: "Lorena Soto",
+					revisor: "Nina Valdivia",
+					future: "Continúa a Proyecto de Ingeniería II",
+					details: "Desarrollo de un sistema de automatización de procesos de compra a proveedores y venta a clientes, además de archivado de transacciones, para gran empresa del retail. Proyecto desarrollado a través de empresa consultora Patito que comprende la fase de diseño y prototipado del sistema.",
+					grade: "<span><strong>   Nota final: </strong> 6.5</span>",
+					committeemembers: "<li>Alejandra Acuña</li><li>Yuri Caceres</li><li>Nestor Gonzalez</li><li>Julian Gil</li><li>Jorge Cornejo</li>",
+					committeecomments: "El trabajo logra con lo esperado de un anteproyecto, y cumple con las condiciones para seguir siendo trabajado en la fase Proyecto de Ingeniería II. Se recomienda tomar en consideración los riesgos de implementar este sistema en el ambiente de producción actual de la empresa."
 				}, 
 				{	
 					src : "images/alicia_soto/2.jpg",
-					title : "Proyecto de Ingeniería II", 
-					description : "Bot para automatizar procesos en empresa consultora.",
+					course : "[2020-01] Proyecto de Ingeniería II", 
+					title : "Bot para automatizar procesos en empresa consultora",
 					status: "Aprobado",
-					bscolor: "success"
+					bscolor: "success",
+					academic: "Lorena Soto",
+					revisor: "Nina Valdivia",
+					future: "Continúa a Proyecto de Ingeniería II",
+					details: "Desarrollo de un sistema de automatización de procesos de compra a proveedores y venta a clientes, además de archivado de transacciones, para gran empresa del retail. Proyecto desarrollado a través de empresa consultora Patito que comprende la fase de diseño y prototipado del sistema.",
+					grade: "<span><strong>   Nota final: </strong> 6.5</span>",
+					committeemembers: "<li>Alejandra Acuña</li><li>Yuri Caceres</li><li>Nestor Gonzalez</li><li>Julian Gil</li><li>Jorge Cornejo</li>",
+					committeecomments: "El trabajo logra con lo esperado de un anteproyecto, y cumple con las condiciones para seguir siendo trabajado en la fase Proyecto de Ingeniería II. Se recomienda tomar en consideración los riesgos de implementar este sistema en el ambiente de producción actual de la empresa."
 				}, 
 				{	
 					src : "images/alicia_soto/at.jpg",
-					title : "Proyecto Aplicado de Título", 
-					description : "Implementación de bot que automatiza procesos en empresa consultora.",
+					course : "[2020-02] Proyecto Aplicado de Título", 
+					title : "Implementación de bot que automatiza procesos en empresa consultora",
 					status: "Pendiente",
-					bscolor: "warning"
+					bscolor: "warning",
+					academic: "Lorena Soto",
+					revisor: "Nina Valdivia",
+					future: "Continúa a Proyecto de Ingeniería II",
+					details: "Desarrollo de un sistema de automatización de procesos de compra a proveedores y venta a clientes, además de archivado de transacciones, para gran empresa del retail. Proyecto desarrollado a través de empresa consultora Patito que comprende la fase de diseño y prototipado del sistema.",
+					grade: "<span><strong>   Nota final: </strong> 6.5</span>",
+					committeemembers: "<li>Alejandra Acuña</li><li>Yuri Caceres</li><li>Nestor Gonzalez</li><li>Julian Gil</li><li>Jorge Cornejo</li>",
+					committeecomments: "El trabajo logra con lo esperado de un anteproyecto, y cumple con las condiciones para seguir siendo trabajado en la fase Proyecto de Ingeniería II. Se recomienda tomar en consideración los riesgos de implementar este sistema en el ambiente de producción actual de la empresa."
 				}
 			]
 		},
@@ -40,7 +61,7 @@ var career = {
 			name : "Carlos", 
 			surname: "Pérez",
 			thumbnail : "images/carlos_perez/photo.jpg",
-			status : "No ha entregado tesis.",
+			status : 'No ha entregado tesis <ion-icon name="alert-outline" class="mr-2 lead text-danger"></ion-icon>',
 			cohort: "2013",
 			curriculum: "ICC110-2009",
 			career: "Ingeniería Civil en Computación e Informática",
@@ -51,33 +72,177 @@ var career = {
 			projects : [
 				{	
 					src : "images/carlos_perez/1.jpg",
-					title : "Proyecto de Ingeniería I", 
-					description : "Aplicación para alimentar gatitos de la calle",
+					course : "Proyecto de Ingeniería I", 
+					title : "Aplicación para alimentar gatitos de la calle",
 					status: "Rechazado",
-					bscolor: "danger"
+					bscolor: "danger",
+					academic: "Lorena Soto",
+					revisor: "Nina Valdivia",
+					future: "Continúa a Proyecto de Ingeniería II",
+					details: "Desarrollo de un sistema de automatización de procesos de compra a proveedores y venta a clientes, además de archivado de transacciones, para gran empresa del retail. Proyecto desarrollado a través de empresa consultora Patito que comprende la fase de diseño y prototipado del sistema.",
+					grade: "<span><strong>   Nota final: </strong> 6.5</span>",
+					committeemembers: "<li>Alejandra Acuña</li><li>Yuri Caceres</li><li>Nestor Gonzalez</li><li>Julian Gil</li><li>Jorge Cornejo</li>",
+					committeecomments: "El trabajo logra con lo esperado de un anteproyecto, y cumple con las condiciones para seguir siendo trabajado en la fase Proyecto de Ingeniería II. Se recomienda tomar en consideración los riesgos de implementar este sistema en el ambiente de producción actual de la empresa."
 				}, 
 				{	
 					src : "images/carlos_perez/1-2.jpg",
-					title : "Proyecto de Ingeniería I (2do intento)", 
-					description : "Aplicación para encontrar tiendas abiertas durante la cuarentena.",
+					course : "Proyecto de Ingeniería I (2do intento)", 
+					title : "Aplicación para encontrar tiendas abiertas durante la cuarentena",
 					status: "Aprobado",
-					bscolor: "success"
+					bscolor: "success",
+					academic: "Lorena Soto",
+					revisor: "Nina Valdivia",
+					future: "Continúa a Proyecto de Ingeniería II",
+					details: "Desarrollo de un sistema de automatización de procesos de compra a proveedores y venta a clientes, además de archivado de transacciones, para gran empresa del retail. Proyecto desarrollado a través de empresa consultora Patito que comprende la fase de diseño y prototipado del sistema.",
+					grade: "<span><strong>   Nota final: </strong> 6.5</span>",
+					committeemembers: "<li>Alejandra Acuña</li><li>Yuri Caceres</li><li>Nestor Gonzalez</li><li>Julian Gil</li><li>Jorge Cornejo</li>",
+					committeecomments: "El trabajo logra con lo esperado de un anteproyecto, y cumple con las condiciones para seguir siendo trabajado en la fase Proyecto de Ingeniería II. Se recomienda tomar en consideración los riesgos de implementar este sistema en el ambiente de producción actual de la empresa."
 				}, 
 				{	
 					src : "images/carlos_perez/2.jpg",
-					title : "Proyecto de Ingeniería II", 
-					description : "Aplicación android de búsqueda de tiendas abiertas durante la pandemia.",
+					course : "Proyecto de Ingeniería II", 
+					title : "Aplicación android de búsqueda de tiendas abiertas durante la pandemia",
 					status: "Aprobado",
-					bscolor: "success"
-                },
+					bscolor: "success",
+					academic: "Lorena Soto",
+					revisor: "Nina Valdivia",
+					future: "Continúa a Proyecto de Ingeniería II",
+					details: "Desarrollo de un sistema de automatización de procesos de compra a proveedores y venta a clientes, además de archivado de transacciones, para gran empresa del retail. Proyecto desarrollado a través de empresa consultora Patito que comprende la fase de diseño y prototipado del sistema.",
+					grade: "<span><strong>   Nota final: </strong> 6.5</span>",
+					committeemembers: "<li>Alejandra Acuña</li><li>Yuri Caceres</li><li>Nestor Gonzalez</li><li>Julian Gil</li><li>Jorge Cornejo</li>",
+					committeecomments: "El trabajo logra con lo esperado de un anteproyecto, y cumple con las condiciones para seguir siendo trabajado en la fase Proyecto de Ingeniería II. Se recomienda tomar en consideración los riesgos de implementar este sistema en el ambiente de producción actual de la empresa."
+				}, 
                 {	
 					src : "images/carlos_perez/at.jpg",
-					title : "Proyecto Aplicado de Título", 
-					description : "Directorio digital de tiendas abiertas durante situaciones de emergencia para teléfonos inteligentes.",
+					course : "Proyecto Aplicado de Título", 
+					title : "Directorio digital de tiendas abiertas durante situaciones de emergencia para teléfonos inteligentes",
 					status: "Atrasado",
-					bscolor: "info"
+					bscolor: "info",
+					academic: "Lorena Soto",
+					revisor: "Nina Valdivia",
+					future: "Continúa a Proyecto de Ingeniería II",
+					details: "Desarrollo de un sistema de automatización de procesos de compra a proveedores y venta a clientes, además de archivado de transacciones, para gran empresa del retail. Proyecto desarrollado a través de empresa consultora Patito que comprende la fase de diseño y prototipado del sistema.",
+					grade: "<span><strong>   Nota final: </strong> 6.5</span>",
+					committeemembers: "<li>Alejandra Acuña</li><li>Yuri Caceres</li><li>Nestor Gonzalez</li><li>Julian Gil</li><li>Jorge Cornejo</li>",
+					committeecomments: "El trabajo logra con lo esperado de un anteproyecto, y cumple con las condiciones para seguir siendo trabajado en la fase Proyecto de Ingeniería II. Se recomienda tomar en consideración los riesgos de implementar este sistema en el ambiente de producción actual de la empresa."
 				}
 			]
-		}
+		},
+		{
+			name : "Daniela", 
+			surname: "Riquelme",
+			thumbnail : "images/daniela_riquelme/photo.jpg",
+			status : 'Egresada <ion-icon name="school-outline" class="mr-2 lead"></ion-icon>',
+			cohort: "2016",
+			curriculum: "ICC110-2014",
+			career: "Ingeniería Civil en Computación e Informática",
+			contactinfo : {
+				phone : "+56911111111",
+				email : "daniela.riquelme@mayor.cl"
+			},
+			projects : [
+				{	
+					src : "",
+					course : "", 
+					title : "",
+					status: "",
+					bscolor: "",
+					academic: "",
+					revisor: "",
+					future: "",
+					details: "",
+					grade: "<span><strong>   Nota final: </strong> 6.5</span>",
+					committeemembers: "<li>Alejandra Acuña</li><li>Yuri Caceres</li><li>Nestor Gonzalez</li><li>Julian Gil</li><li>Jorge Cornejo</li>",
+					committeecomments: ""
+				}
+			]
+		},
+		{
+			name : "Pedro", 
+			surname: "Jimenez",
+			thumbnail : "images/pedro_jimenez/photo.jpg",
+			status : "",
+			cohort: "2016",
+			curriculum: "ICC110-2014",
+			career: "Ingeniería Civil en Computación e Informática",
+			contactinfo : {
+				phone : "",
+				email : ""
+			},
+			projects : [
+				{	
+					src : "",
+					course : "", 
+					title : "",
+					status: "",
+					bscolor: "",
+					academic: "",
+					revisor: "",
+					future: "",
+					details: "",
+					grade: "<span><strong>   Nota final: </strong> 6.5</span>",
+					committeemembers: "<li>Alejandra Acuña</li><li>Yuri Caceres</li><li>Nestor Gonzalez</li><li>Julian Gil</li><li>Jorge Cornejo</li>",
+					committeecomments: ""
+				}
+			]
+		},
+		{
+			name : "Esteban", 
+			surname: "Dido",
+			thumbnail : "images/esteban_dido/photo.jpg",
+			status : "",
+			cohort: "2016",
+			curriculum: "ICC110-2014",
+			career: "Ingeniería Civil en Computación e Informática",
+			contactinfo : {
+				phone : "",
+				email : ""
+			},
+			projects : [
+				{	
+					src : "",
+					course : "", 
+					title : "",
+					status: "",
+					bscolor: "",
+					academic: "",
+					revisor: "",
+					future: "",
+					details: "",
+					grade: "<span><strong>   Nota final: </strong> 6.5</span>",
+					committeemembers: "<li>Alejandra Acuña</li><li>Yuri Caceres</li><li>Nestor Gonzalez</li><li>Julian Gil</li><li>Jorge Cornejo</li>",
+					committeecomments: ""
+				}
+			]
+		},
+		{
+			name : "Natalia", 
+			surname: "Dominguez",
+			thumbnail : "images/natalia_dominguez/photo.jpg",
+			status : "",
+			cohort: "2016",
+			curriculum: "ICC110-2014",
+			career: "Ingeniería Civil en Computación e Informática",
+			contactinfo : {
+				phone : "",
+				email : ""
+			},
+			projects : [
+				{	
+					src : "",
+					course : "", 
+					title : "",
+					status: "",
+					bscolor: "",
+					academic: "",
+					revisor: "",
+					future: "",
+					details: "",
+					grade: "<span><strong>   Nota final: </strong> 6.5</span>",
+					committeemembers: "<li>Alejandra Acuña</li><li>Yuri Caceres</li><li>Nestor Gonzalez</li><li>Julian Gil</li><li>Jorge Cornejo</li>",
+					committeecomments: ""
+				}
+			]
+		},
 	]
 };
