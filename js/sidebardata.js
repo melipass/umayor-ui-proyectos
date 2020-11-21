db02481172c7e78d034631f42c21c884311e84a2
melipass <32807254+melipass@users.noreply.github.com>
2020-11-21 12:31:28 -0300

    Sidebar template to work with if there's time.

---
+++ js/sidebardata.js
@@ -0,0 +1,31 @@
+/* TEST */
+
+var sidebar = {
+    item : [
+        {
+            title : "Página principal",
+            url : "index.html",
+            ionicon : "home"
+        },
+        {
+            title : "Proyectos",
+            url : "proyectos.html",
+            ionicon : "folder"
+        },
+        {
+            title : "Reportes y estadística",
+            url : "",
+            ionicon : "stats"
+        },
+        {
+            title : "Mensajes",
+            url : "",
+            ionicon : "mail"
+        },
+        {
+            title : "Cuentas",
+            url : "",
+            ionicon : "people"
+        }
+    ]
+}
