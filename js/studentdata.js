fc72d1d76f2728747d1fdea4229d3ac8955d8e37
melipass <melissa.silvas@mayor.cl>
2020-11-21 15:05:04 -0300

    Improved projects view.

--- js/studentdata.js
+++ js/studentdata.js
@@ -1,59 +1,81 @@
 var career = {
 	students : [
 		{
-			name : "Alice", 
-			surname: "Smith",
-			thumbnail : "images/alice/photo.jpg",
+			name : "Alicia", 
+			surname: "Soto",
+			thumbnail : "images/alicia_soto/photo.jpg",
+			status : "Esperando respuesta de Profesor Revisor.",
+			cohort: "2016",
+			curriculum: "ICC110-2014",
+			career: "Ingeniería Civil en Computación e Informática",
 			contactinfo : {
 				phone : "+56912345678",
-				email : "alice.smith@mayor.cl"
+				email : "alicia.soto@mayor.cl"
 			},
 			projects : [
 				{	
-					src : "images/alice/1.jpg",
+					src : "images/alicia_soto/1.jpg",
 					title : "Proyecto de Ingeniería I", 
-					description : "Automatización de procesos en empresa consultora."
+					description : "Automatización de procesos en empresa consultora.",
+					status: "Aprobado",
+					bscolor: "success"
 				}, 
 				{	
-					src : "images/alice/2.jpg",
+					src : "images/alicia_soto/2.jpg",
 					title : "Proyecto de Ingeniería II", 
-					description : "Bot para automatizar procesos en empresa consultora."
+					description : "Bot para automatizar procesos en empresa consultora.",
+					status: "Aprobado",
+					bscolor: "success"
 				}, 
 				{	
-					src : "images/alice/at.jpg",
+					src : "images/alicia_soto/at.jpg",
 					title : "Proyecto Aplicado de Título", 
-					description : "Implementación de bot que automatiza procesos en empresa consultora."
+					description : "Implementación de bot que automatiza procesos en empresa consultora.",
+					status: "Pendiente",
+					bscolor: "warning"
 				}
 			]
 		},
 		{
-			name : "Bob", 
-			surname: "Green",
-			thumbnail : "images/bob/photo.jpg",
+			name : "Carlos", 
+			surname: "Pérez",
+			thumbnail : "images/carlos_perez/photo.jpg",
+			status : "No ha entregado tesis.",
+			cohort: "2013",
+			curriculum: "ICC110-2009",
+			career: "Ingeniería Civil en Computación e Informática",
 			contactinfo : {
 				phone : "+56987654321",
-				email : "bob.green@mayor.cl"
+				email : "carlos.perez@mayor.cl"
 			},
 			projects : [
 				{	
-					src : "images/bob/1.jpg",
+					src : "images/carlos_perez/1.jpg",
 					title : "Proyecto de Ingeniería I", 
-					description : "Aplicación para alimentar gatitos de la calle"
+					description : "Aplicación para alimentar gatitos de la calle",
+					status: "Rechazado",
+					bscolor: "danger"
 				}, 
 				{	
-					src : "images/bob/1-2.jpg",
+					src : "images/carlos_perez/1-2.jpg",
 					title : "Proyecto de Ingeniería I (2do intento)", 
-					description : "Aplicación para encontrar tiendas abiertas durante la cuarentena."
+					description : "Aplicación para encontrar tiendas abiertas durante la cuarentena.",
+					status: "Aprobado",
+					bscolor: "success"
 				}, 
 				{	
-					src : "images/bob/2.jpg",
+					src : "images/carlos_perez/2.jpg",
 					title : "Proyecto de Ingeniería II", 
-					description : "Aplicación android de búsqueda de tiendas abiertas durante la pandemia."
+					description : "Aplicación android de búsqueda de tiendas abiertas durante la pandemia.",
+					status: "Aprobado",
+					bscolor: "success"
                 },
                 {	
-					src : "images/bob/at.jpg",
+					src : "images/carlos_perez/at.jpg",
 					title : "Proyecto Aplicado de Título", 
-					description : "Directorio digital de tiendas abiertas durante situaciones de emergencia para teléfonos inteligentes."
+					description : "Directorio digital de tiendas abiertas durante situaciones de emergencia para teléfonos inteligentes.",
+					status: "Atrasado",
+					bscolor: "info"
 				}
 			]
 		}
